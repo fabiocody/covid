4411d02876449fc1eb9b321b0947b19021858666
Fabio Codiglioni <fabiocody@icloud.com>
2021-06-02 22:41:04 +0200

    PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import {HttpClientModule} from '@angular/common/http';
 import {SpinnerComponent} from './elements/spinner/spinner.component';
 import {MaterialModule} from './modules/material.module';
 import * as moment from 'moment';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,12 @@ import * as moment from 'moment';
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'it'},
