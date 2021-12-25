90681f9ece1616942526a087b619e1770b7e5a8b
Fabio Codiglioni <fabiocody@icloud.com>
2021-12-25 21:20:46 +0100

    Add Google Analytics to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UpdateService} from './services/update/update.service';
+import {NavigationEnd, Router} from '@angular/router';
+import {SubSink} from 'subsink';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+declare let gtag: Function;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    title = 'COVID';
+export class AppComponent implements OnInit, OnDestroy {
+    public title = 'COVID';
+    private subs = new SubSink();
 
-    constructor(private updateService: UpdateService) {}
+    constructor(private updateService: UpdateService, private router: Router) {}
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         this.updateService.start();
+        this.subs.sink = this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                gtag('config', 'G-LCT3Z0YENG', {
+                    page_path: event.urlAfterRedirects,
+                });
+            }
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this.subs.unsubscribe();
     }
 }
